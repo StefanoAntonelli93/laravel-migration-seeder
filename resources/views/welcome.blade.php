8d175978220489e05d5fb51105497b82e05bb422
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-10 00:06:25 +0200

    [UPD] create table 'trains' and migration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>laravel-boolando</title>
+    <title>Trains</title>
     <!-- Includiamo gli assets con la direttiva vite-->
     @vite('resources/js/app.js')
 </head>
 
 <body>
-    <h1>Laravel Template</h1>
+    <h1>Trains table</h1>
     <img src="{{ Vite::asset('resources/img/logo.jpg') }}" alt="pic">
 </body>
 
