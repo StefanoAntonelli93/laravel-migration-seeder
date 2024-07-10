3503afb7d98e1854c5bc1fc9005694ed8a53473c
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-11 00:36:22 +0200

    [UPD] add route('home') to Header's Title

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,5 +1,6 @@
 <header class="p-3 text-center bg-success text-white">
-    <h1 class="mt-3 fw-semibold">TRAIN FOR BOOLEAN <i class="fa-solid fa-dragon"></i></h1>
+    <h1 class="mt-3 fw-semibold"><a class="nav-link" href="{{ route('home') }}">TRAIN FOR BOOLEAN <i
+                class="fa-solid fa-dragon"></i></a> </h1>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid bg-success">
             <div class="collapse navbar-collapse" id="navbarNav">
